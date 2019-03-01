dc74303619f5969cfd1dabf2b658b9ec46d98522
Emre Gurbuzturk <48090252+emregurbuzturk@users.noreply.github.com>
2019-03-01 20:07:18 +0300

    Add files via upload
    Kullanıcıdan reel sayı alıyor alınan reel sayının tam ve ondalık kısımlarını buluyor

---
+++ reeltamkisim.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main(void){
+	float sayi,ondalikKisim;
+	int tamKisim;
+	
+	tamKisim = 0;
+	ondalikKisim = 0.0;
+	sayi = 0.0;
+	
+	printf("Bir reel sayi gir ");
+	
+	scanf("%f",&sayi);
+	
+	tamKisim = sayi;
+	
+	ondalikKisim = sayi-tamKisim;
+	
+	printf("Tam kisim %d, ", tamKisim);
+	
+	printf("Ondalik Kisim %f", ondalikKisim);
+	
+	return (0);
+	
+	
+	
+}
